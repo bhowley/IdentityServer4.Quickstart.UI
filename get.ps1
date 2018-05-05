91c921bb60a903a5dd35f29d59ce3f60bd2979e0
bhowley <billyhowley@gmail.com>
2018-05-05 16:40:42 -0400

    Renamed "Quickstart" directory to "Modules".

--- get.ps1
+++ get.ps1
@@ -1,16 +1,16 @@
-$source = "https://github.com/IdentityServer/IdentityServer4.Quickstart.UI/archive/release.zip"
+$source = "https://github.com/bhowley/IdentityServer4.Quickstart.UI/archive/2.2.0.zip"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest $source -OutFile ui.zip
 
-Expand-Archive ui.zip
+Expand-Archive ui.zip ui.temp
 
-if (!(Test-Path -Path Quickstart))  { mkdir Quickstart }
+if (!(Test-Path -Path Modules))  { mkdir Modules }
 if (!(Test-Path -Path Views))       { mkdir Views }
 if (!(Test-Path -Path wwwroot))     { mkdir wwwroot }
 
-copy .\ui\IdentityServer4.Quickstart.UI-release\Quickstart\* Quickstart -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI-release\Modules\* Modules -recurse -force
 copy .\ui\IdentityServer4.Quickstart.UI-release\Views\* Views -recurse -force
 copy .\ui\IdentityServer4.Quickstart.UI-release\wwwroot\* wwwroot -recurse -force
 
 del ui.zip
-del ui -recurse
+del ui.temp -recurse
